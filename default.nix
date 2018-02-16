2904489649684933a1941f12f1d0f603cf060956
Markus Kowalewski <markus.kowalewski@gmail.com>
2018-02-15 22:10:08 -0800

    Clear top level list

--- default.nix
+++ default.nix
@@ -22,18 +22,9 @@ in with pkgs; {
 
   qdng = callPackage ./qdng { };
 
-#  arpackng = callPackage ./arpack-ng {};
-
-#  libxc = callPackage ./libxc {};
-
   mkl = callPackage ./mkl { };
 
   impi = callPackage ./impi { };
 
-#  slurm17 = callPackage ./slurm { } ; # fixed version from the master branch
-#  slurmSpankX11 = callPackage ./slurm-spank-x11 { } ; # make X11 work in srun sessions
-
-# beegfs = callPackage ./beegfs { };
-# beegfs-module = callPackage ./beegfs/kernel-module.nix { kernel=pkgs.linux_4_9; };
-
+  slurmSpankX11 = pkgs.slurmSpankX11; # make X11 work in srun sessions
 }
