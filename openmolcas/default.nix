a5b6c4b86f6daa299073acbc00b07202e80ef86e
Markus Kowalewski <markus.kowalewski@gmail.com>
2018-02-21 23:39:47 -0800

    Fixed openmolcas
    Seems to compile and install now

--- openmolcas/default.nix
+++ openmolcas/default.nix
@@ -1,4 +1,6 @@
-{ stdenv, fetchurl, cmake, gfortran, openblas, hdf5-fortran, python3, texLive } :
+{ stdenv, fetchurl, cmake, gfortran
+, openblas, hdf5-cpp, python3, texLive
+} :
 
 let
   version = "v18.0.o180115-1800";
@@ -8,13 +10,29 @@ in stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://gitlab.com/Molcas/OpenMolcas/repository/${version}/archive.tar.bz2";
-    sha256 = "0nak89s37yl1zlm70jr6qvaw1fvdfhn9m21v4n16gspbrxgw71bz";
+    sha256 = "1y06c51df2a3rqfk83kml1fj4zi47gml90k9gq8787zifycbhyxj";
   };
 
   nativeBuildInputs = [ cmake texLive ];
-  buildInputs = [ gfortran openblas hdf5-fortran python3 ];
+  buildInputs = [ gfortran openblas hdf5-cpp python3 ];
 
-  cmakeFlags = [ "-DOPENMP=ON" "-DLINALG=OpenBLAS" "-DOPENBLASROOT=${openblas}" ];
+  cmakeFlags = [
+    "-DOPENMP=ON"
+    "-DLINALG=OpenBLAS"
+    "-DTOOLS=OFF"
+    "-DHDF5=OFF"
+    "-DCTEST=ON"
+    "-DOPENBLASROOT=${openblas}"
+  ];
+
+  postConfigure = ''
+    mkdir -p $out/bin
+    export PATH=$PATH:$out/bin
+  '';
+
+  postPatch = ''
+    patchShebangs Tools/
+  '';
 
   meta = with stdenv.lib; {
     description = "Quantum chemistry software package";
