17dddb2aa1e8e60e2a2274b5e58d4c9751442d55
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2019-04-15 14:25:44 +0200

    lib: add function writeScriptSlurm

--- default.nix
+++ default.nix
@@ -235,6 +235,34 @@ in with super;
   pmix = callPackage ./pmix { };
 
   ucx = callPackage ./ucx { enableOpt=true; };
+
+  # Utilities
+  writeScriptSlurm =
+    { name
+    , text
+    , N ? 1     # No. of nodes
+    , n ? null  # No. of task
+    , c ? null  # No. of CPUs per task
+    , J ? null  # Job name
+    # shell to use
+    , shell ? "bash"
+    # if set will use nix-shell as script interpreter
+    , nixShellArgs ? null
+    } :
+    with super.lib; super.writeTextFile {
+      inherit name;
+      executable = true;
+      text = ''
+        #!/usr/bin/env ${if nixShellArgs == null then "${shell}" else "nix-shell"}
+        #${optionalString (nixShellArgs != null) "#!nix-shell -i ${shell} ${nixShellArgs}"}
+        #SBATCH -J ${if (J != null) then J else name}
+        #SBATCH -N ${toString N}
+        #${optionalString (n != null) "SBATCH -n ${toString n}"}
+        #${optionalString (c != null) "SBATCH -c ${toString c}"}
+        ${text}
+      '';
+    };
+
 }
 
 
