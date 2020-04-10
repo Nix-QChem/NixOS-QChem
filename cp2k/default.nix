975782210ed7d8083fdeb29e9a4bc14359cd7189
Markus Kowalewski <markus.kowalewski@gmail.com>
2020-04-11 00:47:04 +0200

    cp2k: 6.1.0 -> 7.1.0

--- cp2k/default.nix
+++ cp2k/default.nix
@@ -1,10 +1,11 @@
 { stdenv, pkgs, fetchFromGitHub, python, gfortran, openblasCompat
-, fftw, libint1, libxc, mpi, gsl, scalapack, openssh, makeWrapper
-, libxsmm, optAVX ? true
+, fftw, libint2, libxc, mpi, gsl, scalapack, openssh, makeWrapper
+, libxsmm, which
+, optAVX ? false
 } :
 
 let
-  version = "6.1.0";
+  version = "7.1.0";
 
   cp2kVersion = "psmp";
   arch = "Linux-x86-64-gfortran";
@@ -17,17 +18,18 @@ let
     AR         = ar -r
     DFLAGS     = -D__FFTW3 -D__LIBXC -D__LIBINT -D__parallel -D__SCALAPACK \
                  -D__MPI_VERSION=3 -D__F2008 -D__LIBXSMM \
-                 -D__LIBINT_MAX_AM=7 -D__LIBDERIV_MAX_AM1=6 -D__MAX_CONTR=4
+                 -D__MAX_CONTR=4
 
     FCFLAGS    = $(DFLAGS) -O2 -ffree-form -ffree-line-length-none \
                  -ftree-vectorize -funroll-loops -msse2 \
                  ${stdenv.lib.optionalString optAVX "-mavx -mavx2"} \
                  -std=f2008 \
                  -I${libxc}/include -I${libxsmm}/include \
+                 -I${libint2}/include \
                  -fopenmp -ftree-vectorize -funroll-loops
     LIBS       = -lfftw3 -lfftw3_threads -lfftw3_omp -lscalapack -lopenblas \
                  -lxcf03 -lxc -lxsmmf -lxsmm \
-                 ${libint1}/lib/libderiv.a ${libint1}/lib/libint.a \
+                 -lint2 -lstdc++ \
                  -fopenmp
   '';
 
@@ -38,13 +40,12 @@ in stdenv.mkDerivation rec {
     owner = "cp2k";
     repo = "cp2k";
     rev = "v${version}";
-    sha256 = "1c2f1pqa2basv034dds1lnpswxczhk20kx3vh5w84skmc34v6921";
+    sha256 = "0icmzqc9v8rvcw8xg295qpa7h3d1vpjbc89l0lvakam977ianblf";
+    fetchSubmodules = true;
   };
 
-  patches = [ ./openmpi4.patch ];
-
-  nativeBuildInputs = [ python openssh makeWrapper ];
-  buildInputs = [ gfortran fftw gsl libint1 libxc libxsmm openblasCompat mpi scalapack ];
+  nativeBuildInputs = [ python which openssh makeWrapper ];
+  buildInputs = [ gfortran fftw gsl libint2 libxc libxsmm openblasCompat mpi scalapack ];
 
   makeFlags = [
     "ARCH=${arch}"
@@ -57,15 +58,7 @@ in stdenv.mkDerivation rec {
 
   postPatch = ''
     cp ${config} arch/${arch}.${cp2kVersion}
-    patchShebangs tools
-  '';
-
-  preBuild = ''
-    cd makefiles
-  '';
-
-  postBuild = ''
-    cd ..
+    patchShebangs tools exts/dbcsr/tools/build_utils
   '';
 
   checkPhase = ''
@@ -73,16 +66,17 @@ in stdenv.mkDerivation rec {
     export OMPI_MCA_rmaps_base_oversubscribe=1
     export CP2K_DATA_DIR=data
 
-    for i in libcp2k_unittest dbcsr_test_csr_conversions dbcsr_unittest dbcsr_tensor_unittest; do
-      mpirun -np 2 exe/${arch}/$i.${cp2kVersion}
-    done
+    mpirun -np 2 exe/${arch}/libcp2k_unittest.${cp2kVersion}
   '';
 
   installPhase = ''
     mkdir -p $out/bin $out/share/cp2k
 
     cp exe/Linux-x86-64-gfortran/* $out/bin
-    makeWrapper $out/bin/cp2k.${cp2kVersion} $out/bin/cp2k --set CP2K_DATA_DIR $out/share/cp2k
+
+    for i in cp2k cp2k_shell graph; do
+      makeWrapper $out/bin/$i.${cp2kVersion} $out/bin/$i --set CP2K_DATA_DIR $out/share/cp2k
+    done
 
     cp -r data/* $out/share/cp2k
 
@@ -94,6 +88,7 @@ in stdenv.mkDerivation rec {
     description = "Quantum chemistry and solid state physics program";
     homepage = https://www.cp2k.org;
     license = licenses.gpl3;
+    maintainers = [ maintainters.markuskowa ];
     platforms = platforms.linux;
   };
 }
