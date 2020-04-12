0f353d800df0e5dc8a0b4f8c2e21ac9873d42784
Markus Kowalewski <markus.kowalewski@gmail.com>
2020-04-12 16:51:53 +0200

    ucx: 1.4.0 -> 1.7.0
    sync with upstream derivation

--- ucx/default.nix
+++ ucx/default.nix
@@ -6,7 +6,7 @@
 } :
 
 let
-  version = "1.4.0";
+  version = "1.7.0";
 
 in stdenv.mkDerivation {
   name = "ucx-${version}";
@@ -15,15 +15,20 @@ in stdenv.mkDerivation {
     owner = "openucx";
     repo = "ucx";
     rev = "v${version}";
-    sha256 = "0p126wd8xd7x1fwc78hqmaaykyxyn8kpxd69i549d5c9w2a75j8y";
+    sha256 = "149p8s7jrg7pbbq0hw0qm8va119bsl19q4scgk94vjqliyc1s33h";
   };
 
   nativeBuildInputs = [ autoconf automake libtool doxygen ];
 
   buildInputs = [ numactl rdma-core libbfd libiberty perl zlib ];
 
-  configureFlags = [ "--with-rdmacm=${rdma-core}" ]
-    ++ stdenv.lib.optionals enableOpt  [
+  configureFlags = [
+    "--with-rdmacm=${rdma-core}"
+    "--with-dc"
+    "--with-rc"
+    "--with-dm"
+    "--with-verbs=${rdma-core}"
+  ] ++ stdenv.lib.optionals enableOpt  [
       "--with-avx"
       "--with-sse41"
       "--with-sse42"
@@ -40,6 +45,7 @@ in stdenv.mkDerivation {
     homepage = http://www.openucx.org;
     license = licenses.bsd3;
     platforms = platforms.linux;
+    maintainers = [ maintainers.markuskowa ];
   };
 }
 
