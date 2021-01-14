7ade55467da35366d9c75a63226138752628fb4e
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2021-01-14 17:19:05 +0100

    mvapich: 2.2 -> 2.3.5

--- mvapich/default.nix
+++ mvapich/default.nix
@@ -6,15 +6,21 @@
 } :
 
 let
-  version = "2.2";
+  version = "2.3.5";
 
 in stdenv.mkDerivation {
   name = "mvapich-${version}";
 
   src = fetchurl {
     url = "http://mvapich.cse.ohio-state.edu/download/mvapich/mv2/mvapich2-${version}.tar.gz";
-    sha256 = "0cdi7cxmkfl1zhi0czmzm0mvh98vbgq8nn9y1d1kprixnb16y6kr";
+    sha256 = "130sjr5sv851j87qhxiw0a1wc43v6is07vmyly4im67wqpz6gx7r";
   };
+
+  patches = [
+    # glibc >= 2.32 compatability
+    ./glibc_signals.patch
+  ];
+
 
   nativeBuildInputs = [ pkgconfig bison ];
   propagatedBuildInputs = [ numactl rdma-core zlib infiniband-diags opensm ];
