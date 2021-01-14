ae1e0135ffbaf68b91cbbc509adcebf036011c08
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2021-01-14 17:13:14 +0100

    sos: 1.4.0 -> 1.5.0
    fix test

--- sos/default.nix
+++ sos/default.nix
@@ -1,9 +1,9 @@
 { stdenv, fetchFromGitHub, autoconf, automake, libtool
-, rdma-core, libfabric, libnl, perl, openmpi, openssh
+, rdma-core, libfabric, libnl, perl, mpi, openssh
 } :
 
 let
-  version = "1.4.0";
+  version = "1.5.0";
 
 in stdenv.mkDerivation {
   name = "sos-${version}";
@@ -12,7 +12,7 @@ in stdenv.mkDerivation {
     owner = "Sandia-OpenSHMEM";
     repo = "SOS";
     rev = "v${version}";
-    sha256 = "042zhdi2jr08jaliilkx7gmhhdlrwikkq667d2iki8q553klrn1k";
+    sha256 = "0wj3ijxawpy5i33j7lnrlk4h6fhww0n3cwjfk28fpnxz30spcfwk";
   };
 
   configureFlags = [
@@ -21,7 +21,7 @@ in stdenv.mkDerivation {
     "--with-ofi=${libfabric}"
   ];
 
-  nativeBuildInputs = [ autoconf automake libtool openmpi openssh ];
+  nativeBuildInputs = [ autoconf automake libtool mpi openssh ];
   buildInputs = [ rdma-core libfabric libnl perl ];
 
   enableParallelBuilding = true;
@@ -40,6 +40,8 @@ in stdenv.mkDerivation {
     fi_info
 #export SHMEM_OFI_PROVIDER=sockets
 #export SHMEM_OFI_FABRIC="127.0.0.1/8"
+    export OMPI_MCA_rmaps_base_oversubscribe=1
+    export HYDRA_IFACE=lo
     export SHMEM_OFI_DOMAIN=lo
     make check TEST_RUNNER="mpiexec -n 2"
   '';
