be78f75a348e4afcfc581ebecbb520a34e5f5191
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2021-02-19 14:10:26 +0100

    fix NUR

--- nur.nix
+++ nur.nix
@@ -1,25 +1,12 @@
-{ pkgs ? import <nixpkgs> {} } :
-let
-  # Pin nixpkgs to unstable version that contains the
-  # mpi attribute mod. Remove it later.
-  rev = "5852a21819542e6809f68ba5a798600e69874e76";
-  nixpkgs = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+{ pkgs ? import <nixpkgs> {}
+, lib ? pkgs.lib
+} :
 
+let
   # Only expose top level derivations
   filterDerivations = pkgSet: with pkgs.lib; filterAttrs (name: value: isDerivation value) pkgSet;
 
-  # create the package set
-  pkgsUnstable = (import nixpkgs) {
-    overlays = [ (import ./default.nix) ];
-
-    config = {
-      allowUnfree = true;
-      qchem-config = {
-        allowEnv = true;
-        optAVX = true;
-      };
-    };
-  };
+  pkgsUnstable = lib.fix' (lib.extends (import ./default.nix) (self: pkgs));
 
 in {
   overlays = {
