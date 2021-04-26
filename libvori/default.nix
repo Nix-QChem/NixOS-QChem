ac280e2f0cd69c9f13bc5b1de601572589641e37
Phillip Seeber <phillip.seeber@googlemail.com>
2021-04-26 17:47:19 +0200

    libvori: init at 201229

---
+++ libvori/default.nix
@@ -0,0 +1,22 @@
+{ stdenv, lib, cmake }:
+stdenv.mkDerivation rec {
+  pname = "libvori";
+  version = "201229";
+
+  nativeBuildInputs = [
+    cmake
+  ];
+
+  # Original server is misconfigured and messes up the file compression.
+  src = builtins.fetchTarball {
+    url = "https://www.cp2k.org/static/downloads/${pname}-${version}.tar.gz";
+    sha256 = "0j5f4v380qxaf55zq8ksk9d5xzhiklcwp5fanx9apxs3qhzdlk9z";
+  };
+
+  meta = with lib; {
+    description = "Library for Voronoi intergration of electron densities";
+    license = with licenses; [ lgpl3Only ];
+    homepage = "https://brehm-research.de/libvori.php";
+    platforms = platforms.unix;
+  };
+}
