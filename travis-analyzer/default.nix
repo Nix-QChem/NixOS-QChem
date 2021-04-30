f27e9d60864368e8ad5d8d9f48c640a0683aa9af
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2021-04-30 12:09:47 +0200

    travis-analyzer: fix src/fetch

--- travis-analyzer/default.nix
+++ travis-analyzer/default.nix
@@ -1,15 +1,17 @@
-{ stdenv, lib }:
+{ stdenv, lib, fetchurl }:
+
 stdenv.mkDerivation rec {
     pname = "travis-analyzer";
     version = "03Jun2020";
 
-    src = fetchTarball  {
+    src = fetchurl  {
       url = "http://www.travis-analyzer.de/files/travis-src-200504-hf2.tar.gz";
-      sha256= "1244w0sqb976c038hfkxvj4iym9nfbzhnisscrzx6vdr7q1g8pxh";
+      sha256= "01c5jpaa1mvh0fk0h3ch0hdxd5bqnpf3i68nnql26qjf5q7s5a51";
     };
 
     dontConfigure = true;
     enableParallelBuilding = true;
+
     installPhase = ''
       mkdir -p $out/bin
       cp exe/travis $out/bin/.
