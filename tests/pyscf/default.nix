4c3cec96c3a2156f7b35cc81a9b056de381be1ee
Phillip Seeber <phillip.seeber@googlemail.com>
2021-05-19 15:49:29 +0200

    pyscf: extend tests

--- tests/pyscf/default.nix
+++ tests/pyscf/default.nix
@@ -13,6 +13,7 @@ in batsTest {
   testScript = ''
     @test "PySCF" {
       OMP_NUM_THREADS=$TEST_NUM_CPUS ${python}/bin/python3 input.py > output
+      grep "Total energy of first excited state -75.2377" output
     }
   '';
 }
