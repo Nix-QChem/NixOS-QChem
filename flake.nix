1c13efc002dc9d81c7d9e3302cbb831a4471cc53
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2021-07-07 11:27:51 +0200

    flakes: follow 21.05 release channel

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "NixOS-QChem flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
 
   outputs = { self, nixpkgs } : let
       lib = import "${nixpkgs}/lib";
