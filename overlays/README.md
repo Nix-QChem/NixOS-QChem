a1d9837d87efacba304b0cd96dc7fe3815a77cb9
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2021-11-30 14:44:56 +0100

    overlays: add extra overlay files

---
+++ overlays/README.md
@@ -0,0 +1,5 @@
+This directory contains extra overlays, which modify nixpkgs.
+These overlays need to be inserted before NixOS-QChem.
+
+* cuda.nix: selects latest CUDA toolkit and builds openmpi with CUDA support
+
---
+++ overlays/cuda.nix
@@ -0,0 +1,20 @@
+#
+# Turn relevant CUDA flags and use latest CUDA toolkit
+#
+final: prev:
+
+{
+  cudatoolkit = prev.cudatoolkit_11;
+
+  mpich = prev.mpich.override {
+    ch4backend = final.ucx;
+  };
+
+  openmpi = prev.openmpi.override {
+    cudaSupport = true;
+  };
+
+  ucx = prev.ucx.override {
+    enableCuda = true;
+  };
+}
