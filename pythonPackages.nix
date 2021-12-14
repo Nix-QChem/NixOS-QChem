24f1b521584c381e6c49cd97218e915de2d2dae3
Phillip Seeber <phillip.seeber@googlemail.com>
2021-12-14 16:42:31 +0100

    gau2grid: remove old versions
    cleanup

--- pythonPackages.nix
+++ pythonPackages.nix
@@ -19,8 +19,6 @@ let
     gpaw = callPackage ./pkgs/apps/gpaw { };
 
     gau2grid = callPackage ./pkgs/apps/gau2grid { };
-    gau2grid-1_3_1 = callPackage ./pkgs/apps/gau2grid { version = "1.3.1"; sha256 = "0zkfil7cxjip79wqvhljk1ifjq0cwxzx6wlxgp63b6wbagma0i12"; };
-    gau2grid-2_0_4 = callPackage ./pkgs/apps/gau2grid { version = "2.0.4"; sha256 = "0qypq8iax0n6yfi4223zya468v24b60nr0x43ypmsafj0104zqa6"; };
 
     meep = callPackage ./pkgs/apps/meep { };
 
