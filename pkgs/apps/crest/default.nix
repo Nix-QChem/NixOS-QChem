bab35859a2a36f84fb269d2ae1a841085b0ef581
Phillip Seeber <phillip.seeber@googlemail.com>
2021-12-23 14:44:14 +0100

    crest: 2.11.1 -> 2.11.2
    crest: use normal gfortran
    
    
    crest: fix patch hash

--- pkgs/apps/crest/default.nix
+++ pkgs/apps/crest/default.nix
@@ -1,10 +1,25 @@
-{ stdenv, lib, fetchurl, makeWrapper, cmake
-, gfortran, mkl, fetchFromGitHub, xtb
+{ stdenv, lib, fetchpatch, makeWrapper, cmake
+, gfortran, blas, lapack, fetchFromGitHub, xtb
 } :
 
 stdenv.mkDerivation rec {
   pname = "crest";
-  version = "2.11.1";
+  version = "2.11.2";
+
+  src = fetchFromGitHub {
+    owner = "grimme-lab";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-IdGo8VdUdw2GtWUaQywiJwhR20XNSEsZbGsGCmzsRlg=";
+  };
+
+  patches = [
+    # gfortran compilation issues due to non-standard use of .eq. instead of .eqv.
+    (fetchpatch {
+      url = "https://patch-diff.githubusercontent.com/raw/grimme-lab/crest/pull/92.diff";
+      sha256 = "0sdd7lpnhrrfagidfg438gv011mrmvdlpnnispcvjcbs7zxzzk2a";
+    })
+  ];
 
   nativeBuildInputs = [
     cmake
@@ -12,14 +27,7 @@ stdenv.mkDerivation rec {
     gfortran
   ];
 
-  buildInputs = [ mkl ];
-
-  src = fetchFromGitHub {
-    owner = "grimme-lab";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "/6f5MH+0AHXrVC26KA3fXTrJYuNSfeW3H79dJPMjuRw=";
-  };
+  buildInputs = [ blas lapack ];
 
   FFLAGS = "-ffree-line-length-512";
 
