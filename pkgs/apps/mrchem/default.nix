65302f64ba146fe447e6987961da85bd358ee114
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2022-04-15 12:20:50 +0200

    mrchem: add gcc releated patch

--- pkgs/apps/mrchem/default.nix
+++ pkgs/apps/mrchem/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, cmake
+{ lib, stdenv, fetchFromGitHub, fetchpatch, cmake
 , eigen, xcfun, mpi, nlohmann_json
 , enableMpi ? mrcpp.enableMpi
 , mrcpp
@@ -14,6 +14,12 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     sha256 = "15sdd3kxl5pn3qsdsqryvyfc7fqp0lrgh7gdq4299l3qpr214y9r";
   };
+
+  patches = [ (fetchpatch {
+    name = "gcc-patch";
+    url = "https://github.com/MRChemSoft/mrchem/commit/c1959c688a6e159945d6281599b0de2143c89d25.patch";
+    sha256 = "014vh5wl1l4a5xqxfwacrpb3im7knqjp2vmcbikfnmc1czw0s267";
+  })];
 
   postPatch = ''
     substituteInPlace python/CMakeLists.txt --replace \
