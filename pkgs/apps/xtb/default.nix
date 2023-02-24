2798e01a318cc5c1d289c3baf802125a4954aa3f
Phillip Seeber <phillip.seeber@uni-jena.de>
2023-02-24 11:07:40 +0100

    xtb: 6.5.1 -> 6.6.0
    xtb: blas lapack ILP64 assertions

--- pkgs/apps/xtb/default.nix
+++ pkgs/apps/xtb/default.nix
@@ -1,9 +1,27 @@
-{ stdenv, lib, gfortran, fetchFromGitHub, cmake, makeWrapper, blas, lapack, writeTextFile
-, mctc-lib, test-drive
-, turbomole, enableTurbomole ? false
-, orca, enableOrca ? false
+{ stdenv
+, lib
+, gfortran
+, fetchFromGitHub
+, cmake
+, makeWrapper
+, blas
+, lapack
+, writeTextFile
+, mctc-lib
+, test-drive
+, tblite
+, toml-f
+, simple-dftd3
+, dftd4
+, multicharge
+, enableTurbomole ? false
+, turbomole
+, enableOrca ? false
+, orca
 , cefine
-} :
+}:
+
+assert !blas.isILP64 && !lapack.isILP64;
 
 let
   description = "Semiempirical extended tight-binding program package";
@@ -14,25 +32,35 @@ let
     ++ lib.optional enableTurbomole cefine
   );
 
-in stdenv.mkDerivation rec {
+in
+stdenv.mkDerivation rec {
   pname = "xtb";
-  version = "6.5.1";
+  version = "6.6.0";
 
-  src = fetchFromGitHub  {
+  src = fetchFromGitHub {
     owner = "grimme-lab";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-9DTaHsK1NgcNbPKsjrVNvoWTyLdaqilZ59sAjAudS2M=";
+    hash = "sha256-RjSxuRAddTtSXOBEBiad916w312v3PWNHDLBzRdAjJM=";
   };
 
   nativeBuildInputs = [
     gfortran
     cmake
     makeWrapper
-    test-drive
   ];
 
-  buildInputs = [ blas lapack mctc-lib ];
+  buildInputs = [
+    blas
+    lapack
+    mctc-lib
+    tblite
+    test-drive
+    toml-f
+    simple-dftd3
+    dftd4
+    multicharge
+  ];
 
   hardeningDisable = [ "format" ];
 
@@ -57,13 +85,18 @@ in stdenv.mkDerivation rec {
       --prefix PATH : "${binSearchPath}"
   '';
 
+  doCheck = true;
+  preCheck = ''
+    export OMP_NUM_THREADS=2
+  '';
+
   setupHooks = [ ./xtbHook.sh ];
 
   passthru = { inherit enableOrca enableTurbomole; };
 
   meta = with lib; {
     inherit description;
-    homepage = "https://www.chemie.uni-bonn.de/pctc/mulliken-center/grimme/software/xtb";
+    homepage = "https://github.com/grimme-lab/xtb";
     license = licenses.lgpl3Only;
     platforms = platforms.linux;
     maintainers = [ maintainers.sheepforce ];
