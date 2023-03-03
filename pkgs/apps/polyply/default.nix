e3b63a0bc5588eadc28f7e29e913bd683f82af50
Phillip Seeber <phillip.seeber@uni-jena.de>
2023-03-03 16:37:22 +0100

    polyply: 1.4.0 -> 1.5.0

--- pkgs/apps/polyply/default.nix
+++ pkgs/apps/polyply/default.nix
@@ -1,15 +1,24 @@
-{ lib, buildPythonPackage, fetchFromGitHub, numpy, scipy, vermouth, pbr
-, tqdm, numba, pytestCheckHook }:
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, numpy
+, scipy
+, vermouth
+, pbr
+, tqdm
+, numba
+, pytestCheckHook
+}:
 
 buildPythonPackage rec {
   pname = "polyply";
-  version = "1.4.0";
+  version = "1.5.0";
 
   src = fetchFromGitHub {
     owner = "marrink-lab";
     repo = "polyply_1.0";
     rev = "v${version}";
-    hash = "sha256-JJDIsWCmjf6FxJqzHZC6scPr7iaFgOJYHfZaBnD3xUk=";
+    hash = "sha256-nyyNjuzD9RSzuo5TQyCP/aHqDj3jZ8Tx5sExUXAswCw=";
   };
 
   postPatch = ''
@@ -36,7 +45,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Generate input parameters and coordinates for atomistic and coarse-grained simulations of polymers, ssDNA, and carbohydrates";
     homepage = "https://github.com/marrink-lab/polyply_1.0";
-    maintainers =  [ maintainers.sheepforce ];
+    maintainers = [ maintainers.sheepforce ];
     license = [ licenses.asl20 ];
   };
 }
