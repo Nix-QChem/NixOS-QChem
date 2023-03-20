7551659105d79e1bb3ec87ef26fd8b607f7d486f
Phillip Seeber <phillip.seeber@uni-jena.de>
2023-03-20 11:50:03 +0100

    toml-f: remove in favour of upstream package

--- overlay.nix
+++ overlay.nix
@@ -283,8 +283,6 @@ let
 
         tinker = callPackage ./pkgs/apps/tinker { };
 
-        toml-f = callPackage ./pkgs/lib/toml-f { };
-
         travis-analyzer = callPackage ./pkgs/apps/travis-analyzer { };
 
         turbomole = callPackage ./pkgs/apps/turbomole { };
