9fcbe43aac4955433a7262cf14f5cd9a3c1fa244
Phillip Seeber <phillip.seeber@uni-jena.de>
2024-03-12 10:50:15 +0100

    python3Packages.cclib: 1.7.1 -> 1.8.1b0

--- pkgs/lib/cclib/default.nix
+++ pkgs/lib/cclib/default.nix
@@ -1,32 +1,69 @@
-{ lib, fetchPypi, buildPythonPackage
-, packaging, numpy, scipy, periodictable, pyqt4
+{ buildPythonPackage
+, lib
+, fetchFromGitHub
+, setuptools
+, pytestCheckHook
+, numpy
+, scipy
+, periodictable
+, pyyaml
+, ase
+, openbabel-bindings
+, h5py
+, pyscf
+, iodata
+, psi4
+, pandas
+, biopython
+, pyquante
 }:
 
 buildPythonPackage rec {
   pname = "cclib";
-  version = "1.7.2";
+  version = "1.8.1b0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-uxa0IjgvR+rWX5K2zS2BJAlDbIMG4MWwSQVjN6Ed/Do=";
+  src = fetchFromGitHub {
+    owner = "cclib";
+    repo = pname;
+    rev = "07590622dbd571c31f8b874697ce024908345d9a";
+    hash = "sha256-w9o2kBRS6UqTn4HmBSCvx008uUBzYFRaFk0pfY2nM7I=";
   };
+
+  pyproject = true;
+
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   propagatedBuildInputs = [
     numpy
     scipy
-    packaging
     periodictable
-    pyqt4
+    pyyaml
+    ase
+    openbabel-bindings
+    h5py
+    pyscf
+    iodata
+    psi4
+    pandas
+    biopython
+    pyquante
   ];
 
-  # fails because of a pyqt4 import test
-  doCheck = false;
+  checkInputs = [ pytestCheckHook ];
+
+  disabledTests = [
+    "test_url_io"
+    "test_multi_url_io"
+    "test_url_seek"
+    "test_ccread_url"
+  ];
 
   meta = with lib; {
-    description = "Library for parsing and interpreting the results of computational chemistry packages";
-    homepage = "https://cclib.github.io/";
+    description = "Parsers and algorithms for computational chemistry logfiles";
+    homepage = "https://github.com/cclib/cclib";
     license = licenses.bsd3;
     maintainers = [ maintainers.markuskowa ];
-    broken = true;
   };
 }
