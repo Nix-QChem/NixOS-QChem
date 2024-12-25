ffe0a62a2759e0a2abf0924be1e2ad8e0025aea7
Markus Kowalewski <markus.kowalewski@gmail.com>
2024-12-25 15:01:08 +0100

    scripts: fix flake update script.
    nixUnstable does not exist has not been need for a while now

--- scripts/update_flake.sh
+++ scripts/update_flake.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p nixUnstable
+#!nix-shell -i bash -p
 
 nix --experimental-features 'nix-command flakes' flake update
 git commit -m "update flake.lock" flake.lock
