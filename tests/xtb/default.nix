93110a01eaa82d674100480a5b8ea1e406a6fef2
Phillip Seeber <phillip.seeber@uni-jena.de>
2026-07-22 15:28:09 +0200

    tests.xtb: remove passthru.enableOrca options

--- tests/xtb/default.nix
+++ tests/xtb/default.nix
@@ -4,7 +4,7 @@ batsTest rec {
   name = "xtb";
 
   auxFiles = [ ./RuCO_6.xyz ];
-  outFile = [ "xtb.out" ] ++ lib.optional xtb.passthru.enableOrca "xtb_orca.out";
+  outFile = [ "xtb.out" ];
 
   nativeBuildInputs = [ xtb ];
 
@@ -13,11 +13,6 @@ batsTest rec {
     @test "GFN2-XTB" {
       ${xtb}/bin/xtb RuCO_6.xyz --gfn 2 -u 2 --grad > xtb.out
       grep "GRADIENT NORM               0.2506" xtb.out
-    }
-  '' + lib.strings.optionalString xtb.passthru.enableOrca ''
-    @test "XTB-ORCA" {
-      ${xtb}/bin/xtb RuCO_6.xyz --orca -u 2 --grad > xtb_orca.out
-      grep "gradient norm              0.1155" xtb_orca.out
     }
   '';
 }
